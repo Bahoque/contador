5a54e9710c49f18cec1c318d712049168b45db4e
Bahoque <bahoque.sena@gmail.com>
2025-11-13 00:47:51 -0500

    contador de suma

---
+++ lib/presentation/screen/counterScreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('counter Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              clickCounter.toString(),
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          clickCounter++;
+          setState(() {});
+        },
+        child: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
